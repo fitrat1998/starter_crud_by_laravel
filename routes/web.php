81e3d2f3a628162efa001822a128268a664b845c
fitrat1998 <towmatovft@gmail.com>
2023-09-23 15:51:17 +0500

    cms

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,6 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/',[TaskController::class, 'index']);
+// Route::get('/',[TaskController::class, 'index']);
 // Route::post('/add',[TaskController::class, 'add'])->name('add');
 
